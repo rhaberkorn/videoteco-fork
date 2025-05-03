rhaberkorn/videoteco-fork

commit b5325e00c402ec18034da4b4a4aaaefa87bb1fef

unsigned long is the base type for buffer positions now
* This allows handling files >64kb even on 16-bit DOS.
* A few flags fields could be squashed into bitfields.
* The buffer overview (0EB) has been adapted for DOS.
  We can only show the total amount of allocated
  memory (as by tecmem.c) for the time being.
  Unfortunately, we don't have a total amount of available
  memory since _memavl() is for small data models and
  the sbrk() apparently doesn't grow.

tecbuf.c
@@ -413,10 +413,10 @@ register struct buff_line *lbp;
  * the buffer position resides on.
  */
 struct buff_line *
-buff_find_line( struct buff_header *hbp, int position )
+buff_find_line( struct buff_header *hbp, unsigned long position )
 {
 register struct buff_line *lbp;
-register int i;
+register unsigned long i;
 
     PREAMBLE();
 
@@ -479,10 +479,10 @@ register int i;
 	if(lbp->next_line == NULL){
 	    char panic_string[LINE_BUFFER_SIZE];
 	    sprintf(panic_string,
-		"buff_find_line: position %d specified in buffer %s, z=%d",
+		"buff_find_line: position %lu specified in buffer %s, z=%lu",
 		position,hbp->name,hbp->zee);
 	    tec_panic(panic_string);
-	    printf("NULL ptr %d bytes still to go\n",i);
+	    printf("NULL ptr %lu bytes still to go\n",i);
 	}/* End IF */
 	lbp = lbp->next_line;
     }/* End While */
@@ -514,7 +514,7 @@ register int i;
  */
 int
 buff_find_offset(	struct buff_header *hbp, 
-					struct buff_line *lbp, int position )
+					struct buff_line *lbp, long position )
 {
 
     PREAMBLE();
@@ -546,7 +546,7 @@ buff_find_offset(	struct buff_header *hbp,
  * of characters at any one time.
  */
 int
-buff_contents( struct buff_header *hbp, int position )
+buff_contents( struct buff_header *hbp, long position )
 {
 register struct buff_line *lbp;
 register int i;
@@ -558,7 +558,7 @@ register int i;
     if(position > hbp->zee || position < 0){
 	char panic_string[LINE_BUFFER_SIZE];
 	sprintf(panic_string,
-	    "buff_contents: illegal position %d specified in buffer %s",
+	    "buff_contents: illegal position %ld specified in buffer %s",
 	    position,hbp->name);
 	tec_panic(panic_string);
     }/* End IF */
@@ -585,7 +585,7 @@ register int i;
  */
 int
 buff_cached_contents(	struct buff_header *hbp, 
-						int position, struct position_cache *cache )
+						unsigned long position, struct position_cache *cache )
 {
 register struct buff_line *lbp;
 register int i;
@@ -596,7 +596,7 @@ register int i;
  */
     if(position > hbp->zee || position < 0){
 	char tmp_message[LINE_BUFFER_SIZE];
-	sprintf(tmp_message,"buff_contents: illegal position %d %s Z = %d\n",
+	sprintf(tmp_message,"buff_contents: illegal position %lu %s Z = %lu\n",
 	    position,hbp->name,hbp->zee);
 	tec_panic(tmp_message);
     }/* End IF */
@@ -968,7 +968,7 @@ buff_readfd( struct buff_header *hbp, char *name, int iochan )
  * wishes to write out the contents of the buffer.
  */
 int
-buff_write( struct buff_header *hbp, int chan, int start, int end )
+buff_write( struct buff_header *hbp, int chan, unsigned long start, unsigned long end )
 {
 register int bcount;
 register struct buff_line *lbp;
@@ -1097,7 +1097,7 @@ int max_length;
     for(hbp = buffer_headers; hbp != NULL ; hbp = hbp->next_header){
 	if(hbp->buffer_number <= 0) continue;
 	i = max_length - strlen(hbp->name);
-	snprintf(tmp_buffer,sizeof(tmp_buffer),"<Buffer %-4d> %s%s %s %6d bytes\n",
+	snprintf(tmp_buffer,sizeof(tmp_buffer),"<Buffer %-4d> %s%s %s %6lu bytes\n",
 	    hbp->buffer_number,hbp->name,&padd_buffer[sizeof(padd_buffer)-1-i],
 	    hbp->ismodified ? "(modified)" : "          ",hbp->zee);
 	i = strlen(tmp_buffer);
@@ -1109,7 +1109,7 @@ int max_length;
     for(hbp = buffer_headers; hbp != NULL ; hbp = hbp->next_header){
 	if(hbp->buffer_number >= 0) continue;
 	i = max_length - strlen(hbp->name);
-	snprintf(tmp_buffer,sizeof(tmp_buffer),"<Buffer %-4d> %s%s %s %6d bytes\n",
+	snprintf(tmp_buffer,sizeof(tmp_buffer),"<Buffer %-4d> %s%s %s %6lu bytes\n",
 	    hbp->buffer_number,hbp->name,&padd_buffer[sizeof(padd_buffer)-1-i],
 	    hbp->ismodified ? "(modified)" : "          ",hbp->zee);
 	i = strlen(tmp_buffer);
@@ -1222,7 +1222,7 @@ int max_length;
  * at the buffer's current location.
  */
 int
-buff_insert( struct buff_header *hbp, int position, char *buffer, int length )
+buff_insert( struct buff_header *hbp, unsigned long position, char *buffer, unsigned long length )
 {
 struct buff_header fake_header;
 struct buff_line *fake_line;
@@ -1296,10 +1296,10 @@ register char *cp;
 int
 buff_insert_from_buffer_with_undo(	struct cmd_token *ct,
 									struct buff_header *dbp,
-									int dest_position,
+									unsigned long dest_position,
 									struct buff_header *sbp,
-									int src_position,
-									int length )
+									unsigned long src_position,
+									size_t length )
 {
 register int i;
 struct undo_token *ut = 0;
@@ -1323,21 +1323,21 @@ char outbuf[1024];
 /*
  * Insure that the specified position is legal
  */
-    if(src_position < 0 || src_position > sbp->zee){
+    if(src_position > sbp->zee){
 	char tmp_message[LINE_BUFFER_SIZE];
 	sprintf(
 	    tmp_message,
-	    "buff_insert_from_buffer_with_undo: bad src pos %d %s Z = %d\n",
+	    "buff_insert_from_buffer_with_undo: bad src pos %lu %s Z = %lu\n",
 	    src_position,
 	    sbp->name,sbp->zee
 	);
 	tec_panic(tmp_message);
     }/* End IF */
-    if(dest_position < 0 || dest_position > dbp->zee){
+    if(dest_position > dbp->zee){
 	char tmp_message[LINE_BUFFER_SIZE];
 	sprintf(
 	    tmp_message,
-	    "buff_insert_from_buffer_with_undo: bad dest pos %d %s Z = %d\n",
+	    "buff_insert_from_buffer_with_undo: bad dest pos %lu %s Z = %lu\n",
 	    dest_position,
 	    dbp->name,dbp->zee
 	);
@@ -1734,9 +1734,9 @@ char outbuf[1024];
 int
 buff_insert_with_undo(	struct cmd_token *ct,
 						struct buff_header *hbp,
-						int position,
+						unsigned long position,
 						char *buffer,
-						int length )
+						unsigned long length )
 {
 struct undo_token *ut;
 
@@ -1771,7 +1771,7 @@ struct undo_token *ut;
  */
 int
 buff_insert_char(	struct buff_header *hbp,
-					int position,
+					unsigned long position,
 					char data )
 {
 register struct buff_line *lbp;
@@ -1786,7 +1786,7 @@ register int i,j;
  */
     if(position > hbp->zee){
 	char tmp_message[LINE_BUFFER_SIZE];
-	sprintf(tmp_message,"buff_insert_char: bad position %d %s Z = %d\n",
+	sprintf(tmp_message,"buff_insert_char: bad position %lu %s Z = %lu\n",
 	    position,hbp->name,hbp->zee);
 	tec_panic(tmp_message);
     }/* End IF */
@@ -1913,7 +1913,7 @@ register int i,j;
 int
 buff_insert_char_with_undo(	struct cmd_token *ct,
 							struct buff_header *hbp,
-							int position,
+							unsigned long position,
 							char data )
 {
 struct undo_token *ut;
@@ -1946,8 +1946,8 @@ struct undo_token *ut;
  */
 void
 buff_delete(	struct buff_header *hbp,
-				int position,
-				int count )
+				unsigned long position,
+				unsigned long count )
 {
 
     PREAMBLE();
@@ -1957,7 +1957,7 @@ buff_delete(	struct buff_header *hbp,
  */
     if((position + count) > hbp->zee){
 	char tmp_message[LINE_BUFFER_SIZE];
-	sprintf(tmp_message,"buff_delete: illegal range %d-%d %s Z = %d\n",
+	sprintf(tmp_message,"buff_delete: illegal range %lu-%lu %s Z = %lu\n",
 	    position,position+count,hbp->name,hbp->zee);
 	tec_panic(tmp_message);
     }/* End IF */
@@ -1983,7 +1983,7 @@ buff_delete(	struct buff_header *hbp,
  */
 int
 buff_delete_char(	struct buff_header *hbp,
-					int position )
+					unsigned long position )
 {
 register struct buff_line *lbp;
 struct buff_line *nlbp;
@@ -1996,7 +1996,7 @@ register int i,j;
  */
     if(position >= hbp->zee){
 	char tmp_message[LINE_BUFFER_SIZE];
-	sprintf(tmp_message,"buff_delete_char: bad position %d %s Z = %d\n",
+	sprintf(tmp_message,"buff_delete_char: bad position %lu %s Z = %lu\n",
 	    position,hbp->name,hbp->zee);
 	tec_panic(tmp_message);
     }/* End IF */
@@ -2133,15 +2133,15 @@ register int i,j;
 int
 buff_delete_with_undo(	struct cmd_token *ct,
 						struct buff_header *hbp,
-						int position,
-						int count )
+						unsigned long position,
+						long count )
 {
 register char *cp;
 struct undo_token *ut;
 register struct buff_line *lbp;
 struct buff_line *top_lbp;
-int top_offset,local_count,bulk_position;
-int bytes_deleted_so_far = 0;
+unsigned long top_offset,local_count,bulk_position;
+unsigned long bytes_deleted_so_far = 0;
 
     PREAMBLE();
 
@@ -2332,8 +2332,8 @@ int bytes_deleted_so_far = 0;
  */
 void
 buff_bulk_insert(	struct buff_header *hbp,
-					int position,
-					int count,
+					unsigned long position,
+					long count,
 					struct buff_line *lbp )
 {
 register struct buff_line *olbp;

teccmd.c
@@ -47,8 +47,8 @@ char *teccmd_c_version = "teccmd.c: $Revision: 1.3 $";
 /*
  * Globals
  */
-    int last_search_pos1;
-    int last_search_pos2;
+    unsigned long last_search_pos1;
+    unsigned long last_search_pos2;
     int last_search_status;
     struct tags *current_tags;
 
@@ -102,16 +102,16 @@ char *teccmd_c_version = "teccmd.c: $Revision: 1.3 $";
  */
 int
 cmd_search(
-			int arg1,
-			int arg2,
+			long arg1,
+			long arg2,
 			struct search_buff *search_tbl )
 {
 int count;
 char forwards;
 int status;
-int pos1;
-int pos2;
-int original_dot;
+unsigned long pos1;
+unsigned long pos2;
+unsigned long original_dot;
 
     PREAMBLE();
 
@@ -191,8 +191,8 @@ int original_dot;
  */
 int
 cmd_forward_search(
-					int pos1,
-					int pos2,
+					unsigned long pos1,
+					unsigned long pos2,
 					struct search_buff *search_tbl )
 {
 register struct search_element *ep;
@@ -210,7 +210,7 @@ struct position_cache running_position;
 /*
  * Insure the search string is non-null
  */
-    if(search_tbl->length <= 0){
+    if(!search_tbl->length){
 	error_message("?Null Search String");
 	search_tbl->error_message_given = YES;
 	return(FAIL);
@@ -351,7 +351,7 @@ struct position_cache running_position;
 /*
  * Insure the search string is non-null
  */
-    if(search_tbl->length <= 0){
+    if(!search_tbl->length){
 	error_message("?Null Search String");
 	search_tbl->error_message_given = YES;
 	return(FAIL);
@@ -576,7 +576,7 @@ int new_length;
 int
 compile_search_string( struct search_buff *search_tbl )
 {
-register int position;
+register unsigned long position;
 register char c;
 register char *cp;
 register struct buff_header *qbp;
@@ -1282,7 +1282,7 @@ register int status;
  * number of words.
  */
 int
-cmd_wordmove( int count )
+cmd_wordmove( long count )
 {
     register int c;
 
@@ -1744,7 +1744,7 @@ register int length;
  * \retval FAIL		Command failed. Error message has been displayed.
  */
 int
-cmd_oscmd(struct cmd_token *uct, int arg_count, int arg1, int arg2, char *cp)
+cmd_oscmd(struct cmd_token *uct, int arg_count, unsigned long arg1, unsigned long arg2, char *cp)
 {
 int last_intr_flag;
 int line_cnt,w;
@@ -1960,7 +1960,7 @@ failreap:
  * user to execute operating system commands from within the editor.
  */
 int
-cmd_oscmd(struct cmd_token *uct, int arg_count, int arg1, int arg2, char *cp)
+cmd_oscmd(struct cmd_token *uct, int arg_count, unsigned long arg1, unsigned long arg2, char *cp)
 {
     PREAMBLE();
 
@@ -2053,8 +2053,8 @@ int length;
  */
 int
 cmd_setoptions(
-				int arg1,
-				int arg2,
+				long arg1,
+				long arg2,
 				struct undo_token *uct )
 {
 struct undo_token fake_token;

tecexec.c
@@ -42,9 +42,9 @@ char *tecexec_c_version = "tecexec.c: $Revision: 1.3 $";
     extern struct window *window_list;
     extern struct buff_header *curbuf,*buffer_headers;
     extern struct buff_header *qregister_push_down_list;
-    extern int last_search_pos1,last_search_pos2;
+    extern unsigned long last_search_pos1,last_search_pos2;
     extern int last_search_status;
-    extern int remembered_dot;
+    extern unsigned long remembered_dot;
     extern char intr_flag;
     extern char immediate_execute_flag;
     extern struct window *curwin;
@@ -355,8 +355,8 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
 	    register struct buff_header *hbp;
 	    register struct buff_header *qbp;
 	    register struct buff_line *lbp;
-	    register int i,j;
-	    int pos1,pos2;
+	    register long i,j;
+	    long pos1,pos2;
 
 	    hbp = curbuf;
 /*
@@ -647,17 +647,17 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
 		return(FAIL);
 	    }/* End IF */
 
-	    sprintf(tmp_buffer,"value = %d",ct->ctx.iarg1);
+	    sprintf(tmp_buffer,"value = %ld",ct->ctx.iarg1);
 	    screen_message(tmp_buffer);
 	    return(SUCCESS);
 
 	case EXEC_C_TWO_EQUALS:
-	    sprintf(tmp_buffer,"value = 0%o",ct->ctx.iarg1);
+	    sprintf(tmp_buffer,"value = 0%lo",ct->ctx.iarg1);
 	    screen_message(tmp_buffer);
 	    return(SUCCESS);
 
 	case EXEC_C_THREE_EQUALS:
-	    sprintf(tmp_buffer,"value = 0x%X",ct->ctx.iarg1);
+	    sprintf(tmp_buffer,"value = 0x%lX",ct->ctx.iarg1);
 	    screen_message(tmp_buffer);
 	    return(SUCCESS);
 /*
@@ -666,7 +666,7 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
  */
 	case EXEC_C_BACKSLASH:
 	    {/* Local Block */
-	    register int i,j;
+	    register long i,j;
 	    char *cp;
 	    int length = 0;
 
@@ -774,7 +774,7 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
  */
 	case EXEC_C_CHAR:
 	    {/* Local Block */
-	    register int i;
+	    register long i;
 
 	    i = curbuf->dot + 1;
 
@@ -798,7 +798,7 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
  */
 	case EXEC_C_RCHAR:
 	    {/* Local Block */
-	    register int i;
+	    register long i;
 
 	    i = curbuf->dot - 1;
 	    if(ct->ctx.iarg1_flag == YES){
@@ -821,7 +821,7 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
  */
 	case EXEC_C_DELETE:
 	    {/* Local Block */
-	    register int i;
+	    register long i;
 
 	    i = 1;
 	    if(ct->ctx.iarg1_flag == YES) i = ct->ctx.iarg1;
@@ -856,7 +856,7 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
  */
 	case EXEC_C_WORD:
 	    {/* Local Block */
-	    register int count;
+	    register long count;
 
 	    ut = allocate_undo_token(uct);
 	    if(ut == NULL) return(FAIL);
@@ -878,9 +878,9 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
  */
 	case EXEC_C_DELWORD:
 	    {/* Local Block */
-	    register int count;
-	    int original_position;
-	    int pos;
+	    register long count;
+	    unsigned long original_position;
+	    unsigned long pos;
 
 	    ut = allocate_undo_token(uct);
 	    if(ut == NULL) return(FAIL);
@@ -912,9 +912,9 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
  */
 	case EXEC_C_RDELWORD:
 	    {/* Local Block */
-	    register int count;
-	    int original_position;
-	    int pos;
+	    register long count;
+	    unsigned long original_position;
+	    unsigned long pos;
 
 	    ut = allocate_undo_token(uct);
 	    if(ut == NULL) return(FAIL);
@@ -1047,8 +1047,8 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
 	    {/* Local Block */
 	    register struct buff_header *hbp;
 	    register struct buff_line *lbp;
-	    register int i,j;
-	    int pos1,pos2;
+	    register long i,j;
+	    long pos1,pos2;
 
 	    hbp = curbuf;
 /*
@@ -1227,7 +1227,7 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
  */
 	case EXEC_C_SEARCH:
 	    {/* Local Block */
-	    int arg1,arg2;
+	    long arg1,arg2;
 #if 0
 	    struct buff_header *qbp;
 #endif
@@ -1253,13 +1253,13 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
 	    if(ct->ctx.iarg1_flag && ct->ctx.iarg2_flag){
 		if(arg1 < 0 || arg1 > curbuf->zee){
 		    sprintf(tmp_message,
-			"?Illegal buffer position %d in search command",arg1);
+			"?Illegal buffer position %ld in search command",arg1);
 		    error_message(tmp_message);
 		    return(FAIL);
 		}/* End IF */
 		if(arg2 < 0 || arg2 > curbuf->zee){
 		    sprintf(tmp_message,
-			"?Illegal buffer position %d in search command",arg2);
+			"?Illegal buffer position %ld in search command",arg2);
 		    error_message(tmp_message);
 		    return(FAIL);
 		}/* End IF */
@@ -1299,7 +1299,7 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
 	case EXEC_C_FRREPLACE:
 	    {/* Local Block */
 	    register struct buff_header *qbp;
-	    register int j;
+	    register unsigned long j;
 
 	    if(last_search_status == 0){
 		if(ct->ctx.flags & CTOK_M_COLON_SEEN){
@@ -1359,7 +1359,7 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
  */
 	case EXEC_C_FSREPLACE1:
 	    {/* Local Block */
-	    register int j;
+	    register unsigned long j;
 	    register struct buff_header *qbp;
 
 	    ct->ctx.tmpval = 0;

commit 9c2afb86acd2e28bef1767b7333acfb9666ec86b

various data type improvements
* fixes at least <FS>, which has been broken since b5325e00c402ec18034da4b4a4aaaefa87bb1fef.
* but probably many other commands as well in the DOS version when working with
  "very large" documents
* now compiles cleanly with -Wsign-compare

tecbuf.c
@@ -555,7 +555,7 @@ register int i;
 /*
  * Insure that the specified position is legal
  */
-    if(position > hbp->zee || position < 0){
+    if((unsigned long)position > hbp->zee || position < 0){
 	char panic_string[LINE_BUFFER_SIZE];
 	sprintf(panic_string,
 	    "buff_contents: illegal position %ld specified in buffer %s",
@@ -585,7 +585,7 @@ register int i;
  */
 int
 buff_cached_contents(	struct buff_header *hbp, 
-						unsigned long position, struct position_cache *cache )
+						long position, struct position_cache *cache )
 {
 register struct buff_line *lbp;
 register int i;
@@ -594,7 +594,7 @@ register int i;
 /*
  * Insure that the specified position is legal
  */
-    if(position > hbp->zee || position < 0){
+    if(position < 0 || (unsigned long)position > hbp->zee){
 	char tmp_message[LINE_BUFFER_SIZE];
 	sprintf(tmp_message,"buff_contents: illegal position %lu %s Z = %lu\n",
 	    position,hbp->name,hbp->zee);
@@ -862,7 +862,7 @@ buff_readfd( struct buff_header *hbp, char *name, int iochan )
     register int i;
     register struct buff_line *lbp;
     struct buff_line *olbp;
-    int original_zee = hbp->zee;
+    unsigned long original_zee = hbp->zee;
     int error_status = SUCCESS;
 
     PREAMBLE();
@@ -1301,16 +1301,16 @@ buff_insert_from_buffer_with_undo(	struct cmd_token *ct,
 									unsigned long src_position,
 									size_t length )
 {
-register int i;
+register unsigned int i;
 struct undo_token *ut = 0;
 struct buff_line *dlbp,*sb_lbp,*se_lbp,*nlbp;
 int doff,sb_off,se_off;
 int newline_included_in_src_data;
-int bytes_to_copy_from_source;
-int bytes_required_for_line,bytes_to_allocate;
+size_t bytes_to_copy_from_source;
+size_t bytes_required_for_line,bytes_to_allocate;
 char *new_buffer;
 register char *dcp,*scp;
-int bytes_inserted_so_far = 0;
+size_t bytes_inserted_so_far = 0;
 
 #ifdef DEBUG
 char outbuf[1024];
@@ -1498,7 +1498,7 @@ char outbuf[1024];
 		char tmp_message[1024];
 		sprintf(
 		    tmp_message,
-		    "buff_insert_from_buffer_with_undo need %d alloced %d\n",
+		    "buff_insert_from_buffer_with_undo need %lu alloced %lu\n",
 		    bytes_required_for_line,
 		    bytes_to_allocate
 		);
@@ -1622,7 +1622,7 @@ char outbuf[1024];
 		char tmp_message[1024];
 		sprintf(
 		    tmp_message,
-		    "buff_insert_from_buffer_with_undo needs %d alloced %d\n",
+		    "buff_insert_from_buffer_with_undo needs %lu alloced %lu\n",
 		    bytes_required_for_line,
 		    bytes_to_allocate
 		);
@@ -1856,7 +1856,7 @@ register int i,j;
 	    cp = lbp->buffer + i;
 	    ocp = nlbp->buffer;
 
-	    while(nlbp->byte_count < j){
+	    while(nlbp->byte_count < (unsigned int)j){
 		*ocp++ = *cp++;
 		nlbp->byte_count++;
 		lbp->byte_count--;
@@ -1988,7 +1988,8 @@ buff_delete_char(	struct buff_header *hbp,
 register struct buff_line *lbp;
 struct buff_line *nlbp;
 register char *cp,*ocp;
-register int i,j;
+register int i;
+register unsigned int j;
 
     PREAMBLE();
 /*
@@ -2134,13 +2135,14 @@ int
 buff_delete_with_undo(	struct cmd_token *ct,
 						struct buff_header *hbp,
 						unsigned long position,
-						long count )
+						unsigned long count )
 {
 register char *cp;
 struct undo_token *ut;
 register struct buff_line *lbp;
 struct buff_line *top_lbp;
-unsigned long top_offset,local_count,bulk_position;
+unsigned long top_offset,bulk_position;
+long local_count;
 unsigned long bytes_deleted_so_far = 0;
 
     PREAMBLE();
@@ -2195,7 +2197,7 @@ unsigned long bytes_deleted_so_far = 0;
  * removed in bulk. lbp gets left pointing at the final line which can be bulk
  * removed.
  */
-    if(lbp && lbp->byte_count <= local_count){
+    if(lbp && (ssize_t)lbp->byte_count <= local_count){
 /*
  * We will get to bulk delete at least one line, so allocate an undo buffer.
  * Point the undo token at the head of the list of line buffers. Remember
@@ -2221,7 +2223,7 @@ unsigned long bytes_deleted_so_far = 0;
 		lbp->format_line = NULL;
 	    }/* End IF */
 	    if(lbp->next_line == NULL) break;
-	    if(lbp->next_line->byte_count > local_count) break;
+	    if((ssize_t)lbp->next_line->byte_count > local_count) break;
 	    lbp = lbp->next_line;
 	}/* End While */
 /*

teccmd.c
@@ -198,7 +198,7 @@ cmd_forward_search(
 register struct search_element *ep;
 register unsigned char buffer_char;
 register int table_position;
-int dotpos;
+unsigned long dotpos;
 char token_match;
 char tmp_message[LINE_BUFFER_SIZE];
 struct position_cache base_position;
@@ -254,7 +254,7 @@ struct position_cache running_position;
 		break;
 	    case SEARCH_C_QREGCHARS:
 		{ struct buff_header *qbp;
-		register int i;
+		register unsigned long i;
 		qbp = buff_qfind(ep->value,0);
 		token_match = 0;
 		if(qbp == NULL) break;
@@ -292,7 +292,7 @@ struct position_cache running_position;
 	if(token_match){
 	    table_position += 1;
 	    ep++;
-	    if(table_position == search_tbl->length){
+	    if((size_t)table_position == search_tbl->length){
 		curbuf->dot = last_search_pos2 = dotpos;
 		{/* Local Block */
 		    register struct buff_header *hbp;
@@ -332,14 +332,14 @@ struct position_cache running_position;
  */
 int
 cmd_reverse_search(
-					int pos1,
-					int pos2,
+					unsigned long pos1,
+					unsigned long pos2,
 					struct search_buff *search_tbl )
 {
 register struct search_element *ep;
 register unsigned char buffer_char;
 register int table_position;
-int dotpos;
+unsigned long dotpos;
 char token_match;
 char tmp_message[LINE_BUFFER_SIZE];
 struct position_cache base_position;
@@ -366,7 +366,7 @@ struct position_cache running_position;
 
     while(dotpos > pos1){
 
-	if(table_position == (search_tbl->length - 1)){
+	if((size_t)table_position == (search_tbl->length - 1)){
 	    last_search_pos2 = dotpos;
 	    base_position = running_position;
 	}/* End IF */
@@ -398,7 +398,7 @@ struct position_cache running_position;
 		break;
 	    case SEARCH_C_QREGCHARS:
 		{ struct buff_header *qbp;
-		register int i;
+		register unsigned long i;
 		qbp = buff_qfind(ep->value,0);
 		token_match = 0;
 		if(qbp == NULL) break;
@@ -478,9 +478,10 @@ set_search_string_with_undo(
 {
 register struct buff_header *qbp;
 register struct undo_token *ut;
-register int i,c;
+register unsigned long i;
+register int c;
 register char *cp;
-int new_length;
+unsigned long new_length;
 
     PREAMBLE();
 

tecdisp.c
@@ -2394,7 +2394,7 @@ struct buff_header *hbp = wptr->win_buffer;
 
     sbp = format_line_free_list;
     if(sbp != NULL){
-	if(sbp->fmt_buffer_size == term_columns){
+	if(sbp->fmt_buffer_size == (size_t)term_columns){
 	    format_line_free_list = sbp->fmt_next_line;
 	}
 	else {

tecexec.c
@@ -1250,13 +1250,13 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
  * insure that the two positions specified are valid buffer positions.
  */
 	    if(ct->ctx.iarg1_flag && ct->ctx.iarg2_flag){
-		if(arg1 < 0 || arg1 > curbuf->zee){
+		if(arg1 < 0 || (unsigned long)arg1 > curbuf->zee){
 		    sprintf(tmp_message,
 			"?Illegal buffer position %ld in search command",arg1);
 		    error_message(tmp_message);
 		    return(FAIL);
 		}/* End IF */
-		if(arg2 < 0 || arg2 > curbuf->zee){
+		if(arg2 < 0 || (unsigned long)arg2 > curbuf->zee){
 		    sprintf(tmp_message,
 			"?Illegal buffer position %ld in search command",arg2);
 		    error_message(tmp_message);
@@ -1410,7 +1410,7 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
  * previous one. Only if we get a miscompare do we delete the contents of
  * the replacement q-register, and insert the new string.
  */
-	    if(qbp->zee > ct->ctx.tmpval){
+	    if((long)qbp->zee > ct->ctx.tmpval){
 		if(ct->input_byte == buff_contents(qbp,ct->ctx.tmpval)){
 		    ct->ctx.tmpval += 1;
 		    return(SUCCESS);
@@ -1433,7 +1433,7 @@ char tmp_buffer[LINE_BUFFER_SIZE],tmp_message[LINE_BUFFER_SIZE];
 		return(FAIL);
 	    }/* End IF */
 
-	    if(qbp->zee > ct->ctx.tmpval){
+	    if((long)qbp->zee > ct->ctx.tmpval){
 		buff_delete_with_undo(uct,qbp,ct->ctx.tmpval,
 		    qbp->zee-ct->ctx.tmpval);
 	    }

teco.h
@@ -648,8 +648,8 @@ void screen_free_format_lines(struct format_line *sbp);
 int buff_delete_char(struct buff_header *hbp,unsigned long position);
 int compile_search_string(struct search_buff *search_tbl);
 int cmd_forward_search(unsigned long pos1,unsigned long pos2,struct search_buff *search_tbl);
-int cmd_reverse_search(int pos1,int pos2,struct search_buff *search_tbl);
-int buff_cached_contents(struct buff_header *,unsigned long,struct position_cache *);
+int cmd_reverse_search(unsigned long pos1,unsigned long pos2,struct search_buff *search_tbl);
+int buff_cached_contents(struct buff_header *,long,struct position_cache *);
 int buff_contents(struct buff_header *hbp,long position);
 void pause_while_in_input_wait(void);
 void restore_tty(void);

tecparse.c
@@ -487,7 +487,7 @@ register struct cmd_token *last_token;
 int c = 0;
 int status;
 char token_used;
-int i;
+unsigned long i;
 char old_modified_state;
 struct buff_header *old_curbuf;
 struct undo_token *ut;
@@ -1207,10 +1207,10 @@ parser_replace_command_line( struct buff_header *qbp )
 {
 register struct cmd_token *ct;
 register struct cmd_token *first_different_ct;
-register int c;
+register unsigned long c;
 char temp;
 register char *command_buffer;
-register int cb_zee;
+register unsigned long cb_zee;
 
     PREAMBLE();
 
@@ -1491,9 +1491,9 @@ char tmp_message[LINE_BUFFER_SIZE];
 
     illegal_position = 0;
     if(pos1 < 0) illegal_position = 1;
-    if(pos2 < 0) illegal_position = 1;
-    if(pos1 > curbuf->zee) illegal_position = 1;
-    if(pos2 > curbuf->zee) illegal_position = 1;
+    else if(pos2 < 0) illegal_position = 1;
+    else if((unsigned long)pos1 > curbuf->zee) illegal_position = 1;
+    else if((unsigned long)pos2 > curbuf->zee) illegal_position = 1;
 
     if(illegal_position == 0) return(0);
 

tecparse.h
@@ -300,7 +300,7 @@ struct undo_token {
 int cmd_oscmd(struct cmd_token *,int,unsigned long,unsigned long,char *);
 int buff_insert_from_buffer_with_undo( struct cmd_token *,
 			struct buff_header *,unsigned long,struct buff_header *,unsigned long,size_t);
-int buff_delete_with_undo( struct cmd_token *,struct buff_header *,unsigned long,long);
+int buff_delete_with_undo( struct cmd_token *,struct buff_header *,unsigned long,unsigned long);
 int buff_insert_with_undo( struct cmd_token *,
 					struct buff_header *,unsigned long,char *,unsigned long);
 int rename_edit_buffer(struct buff_header *,char *,struct cmd_token *);